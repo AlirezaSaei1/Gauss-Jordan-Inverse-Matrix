bf0947b856e860418441560bb5837afa58a7bd92
Alireza Saei <saeialireza359@gmail.com>
2022-06-30 21:58:06 +0430

    Row Interchange added when pivot is 0

--- GaussJordan.py
+++ GaussJordan.py
@@ -1,14 +1,23 @@
 def inverse(matrix, n):
-    pass
+    # Row Interchange
+    for i in range(n):
+        # matrix[i][i] == 0
+        if abs(matrix[i][i]) < 1.0e-12:
+            for j in range(i+1, n):
+                if abs(matrix[j][i]) > abs(matrix[i][i]):
+                        matrix[i], matrix[j] = matrix[j], matrix[i]
+
+        
 
 
-def printMatrix(matrix, label):
-    print(label + ": ")
-    for row in matrix:
-        print(*row)
+def printMatrix(matrix, a, b, label="Matrix"):
+    # print first a rows and b columns of given matrix
+    print(label + ":")
+    for row in range(a):
+        print(*matrix[row][:b])
 
 
-def augmented_matrix(matrix, n):
+def add_identity_matrix(matrix, n):
     # make n*n matrix to n*2n (Matrix + Identity Matrix)
     aug_matrix = [[0 for _ in range(2*n)] for _ in range(n)]
     for i in range(n):
@@ -30,6 +39,6 @@ def readFromFile(address):
 
 matrix = readFromFile("Matrix1.txt")
 n = len(matrix)
-printMatrix(matrix, "Matrix")
-aug_matrix = augmented_matrix(matrix, n)
-printMatrix(aug_matrix, "Augmented matrix")
+printMatrix(matrix, n, n, "Matrix")
+aug_matrix = add_identity_matrix(matrix, n)
+printMatrix(aug_matrix, n, 2*n, "Extended Matrix")
